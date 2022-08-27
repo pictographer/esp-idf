9c177b1ed27c3da64e62e9f01888c41bf1b3809a
John D. Corbett <corbett@pictographer.com>
2022-08-26 18:52:44 -0700

    Added LED pin definitions for the ESP32-C3-13/13U-Kit 3299

--- examples/peripherals/ledc/ledc_basic/main/ledc_basic_example_main.c
+++ examples/peripherals/ledc/ledc_basic/main/ledc_basic_example_main.c
@@ -12,7 +12,12 @@
 
 #define LEDC_TIMER              LEDC_TIMER_0
 #define LEDC_MODE               LEDC_LOW_SPEED_MODE
-#define LEDC_OUTPUT_IO          (5) // Define the output GPIO
+#define LEDC_OUTPUT_IO_RED      (3)
+#define LEDC_OUTPUT_IO_GREEN    (4)
+#define LEDC_OUTPUT_IO_BLUE     (5)
+#define LEDC_OUTPUT_IO_ORANGE   (18)
+#define LEDC_OUTPUT_IO_WHITE    (19)
+#define LEDC_OUTPUT_IO          LEDC_OUTPUT_IO_RED // Define the output GPIO
 #define LEDC_CHANNEL            LEDC_CHANNEL_0
 #define LEDC_DUTY_RES           LEDC_TIMER_13_BIT // Set duty resolution to 13 bits
 #define LEDC_DUTY               (4095) // Set duty to 50%. ((2 ** 13) - 1) * 50% = 4095
